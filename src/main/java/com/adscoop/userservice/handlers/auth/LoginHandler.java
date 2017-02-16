bc0b4d85c57ce307387fecc39c081b180d31684c
Tim Gundmann <tim@gundmann.dk>
2017-02-16 21:27:43 +0100

    logging added

--- src/main/java/com/adscoop/userservice/handlers/auth/LoginHandler.java
+++ src/main/java/com/adscoop/userservice/handlers/auth/LoginHandler.java
@@ -6,6 +6,9 @@ import static ratpack.jackson.Jackson.json;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.adscoop.userservice.congfig.UserModel;
 import com.adscoop.userservice.entites.UserNode;
 import com.adscoop.userservice.services.impls.IAuthorazationService;
@@ -18,6 +21,8 @@ import ratpack.handling.Handler;
  * Created by thokle on 12/12/2016.
  */
 public class LoginHandler implements Handler {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(LoginHandler.class);
 
     private IAuthorazationService authorazationService;
 
@@ -32,9 +37,11 @@ public class LoginHandler implements Handler {
         ctx.parse(fromJson(UserModel.class)).then(userModel -> {
             Optional<UserNode> st = authorazationService.login(userModel.getEmail(), userModel.getPassword());
             if (st.isPresent()) {
+            	LOGGER.debug("Logging in with " + json(st.get()));
                 ctx.getResponse().getHeaders().add("usertoken", st.get().getToken());
                 ctx.render(json(st.get()));
             } else {
+            	LOGGER.debug("Invalid username or password for " + userModel.getEmail());
                 ctx.render("Invalid username or password");
             }
         });
