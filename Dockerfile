32f561eab98f0231b0142188f1c3d12c25ec6df8
Tim Gundmann <tim@gundmann.dk>
2017-02-18 19:51:22 +0100

    log file changed

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM java:8-jdk
+FROM java:8-jdk-alpine
 
 COPY build/distributions/user-service.zip /usr/lib/adscoop/user-service.zip
 
-RUN apt-get update 
+RUN apk update && apk upgrade
+RUN apk add bash
 
 RUN cd /usr/lib/adscoop/ && unzip user-service.zip
 RUN rm /usr/lib/adscoop/user-service.zip
--- src/main/java/com/adscoop/userservice/handlers/CORSHandler.java
+++ src/main/java/com/adscoop/userservice/handlers/CORSHandler.java
@@ -12,8 +12,6 @@ CORSHandler implements Handler {
 	public static final String ACCESS_CONTROL_ALLOW_ORIGIN_VALUE = "*";
 	public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
 
-
-
 	@Override
 	public void handle(Context ctx) throws Exception {
 		MutableHeaders headers = ctx.getResponse().getHeaders();
