20d2f9728904244550eba480479e0a6c17499aa6
= <=>
2017-04-25 12:37:47 +0200

    rewrite unit test

--- src/test/java/com/adscoop/userservice/handlers/users/ActivateHandlerTest.java
+++ src/test/java/com/adscoop/userservice/handlers/users/ActivateHandlerTest.java
@@ -1,87 +1,81 @@
 package com.adscoop.userservice.handlers.users;
 
-import static org.hamcrest.CoreMatchers.any;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.*;
+import com.adscoop.userservice.entites.UserNode;
 
-import java.util.Optional;
-
-import com.sun.nio.sctp.HandlerResult;
+import com.adscoop.userservice.services.impls.IUser;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
-import com.adscoop.userservice.entites.UserNode;
-import com.adscoop.userservice.handlers.utils.ContextBuilder;
-import com.adscoop.userservice.services.impls.IUser;
-
 import ratpack.exec.Promise;
-import ratpack.handling.Context;
-import ratpack.http.TypedData;
-import ratpack.test.exec.ExecHarness;
+import ratpack.http.Status;
+import ratpack.http.internal.DefaultStatus;
 import ratpack.test.handling.HandlingResult;
 import ratpack.test.handling.RequestFixture;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ActivateHandlerTest {
 
-	@Mock
-	private IUser userService;
-	
-	private ActivateHandler activateHandler;
-	
-	@Before
-	public void setUp() {
-		activateHandler = new ActivateHandler(userService);
-	}
+    @Mock
+    private IUser userService;
 
-	@Ignore
-	@Test
-	public void verifyThatAUserIsActivated() throws Exception {
-		try (ExecHarness execHarness = ExecHarness.harness()) {
-			// given
-			UserNode userNode = UserNode.builder().email("activate@activated-dl").activated(true).build();
+    private ActivateHandler activateHandler;
 
-		when(userService.findByUserToken(anyString())).thenReturn(  Promise.value(userNode));
+    @Before
+    public void setUp() {
+        activateHandler = new ActivateHandler(userService);
+    }
 
 
-			HandlingResult handlingResult =  RequestFixture.handle(new ActivateHandler(userService), requestFixture -> {
-				requestFixture.method("POST").header("content-type", "application/json").uri("user/activate");
+    @Test
+    public void verifyThatAUserIsActivated() throws Exception {
 
-			});
-			// when
+        // given
+        UserNode userNode = UserNode.builder().email("activate@activated-dl").activated(true).build();
 
-			//
-			assertEquals(200,handlingResult.getStatus());
-
-			//verify(userService,times(1)).findByUserToken(anyString());
+        when(userService.findByUserToken(anyString())).thenReturn(Promise.value(userNode));
 
 
-							}
-	}
-	@Ignore
-	@Test
-	public void verifyThatIfNoUserActivationExistAnErrorIsReturnd() throws Exception {
-		// given
-		doReturn(Promise.value(any(UserNode.class)))
-			.when(userService)
-			.findByUserToken(anyString());
-		
-		Context ctx = ContextBuilder.aBuilder().build();
+        HandlingResult handlingResult = RequestFixture.handle(new ActivateHandler(userService), requestFixture -> {
+            requestFixture.method("POST").header("content-type", "application/json").uri("user/activate");
 
-		// when
-		activateHandler.verifyActivation(ctx).toConsumer().accept(mock(TypedData.class));
-		
-		// when
-		verify(ctx).clientError(eq(410));
-	}
-	
-	
+        });
+        // when
+
+        //
+        assertEquals(Status.OK, handlingResult.getStatus());
+
+        verify(userService, times(1)).findByUserToken(anyString());
+
+
+    }
+
+
+
+    @Test
+    public void verifyThatIfNoUserActivationExistAnErrorIsReturnd() throws Exception {
+        // given
+       UserNode userNode = UserNode.builder().build();
+
+       when(userService.findByUserToken(anyString())).thenReturn(Promise.value(userNode));
+
+
+       HandlingResult handlingResult =  RequestFixture.handle(new ActivateHandler(userService), requestFixture -> {
+           requestFixture.method("POST").header("content-type", "application/json").header("token","232323effmdvæmv21e1").uri("user/activate");
+
+        });
+
+
+
+        assertEquals(DefaultStatus,handlingResult.getStatus());
+
+    }
+
+
 }
